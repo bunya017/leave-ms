cd1e3673523e65ce80d9576bca598224e81d591b
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-06 11:43:19 +0100

    refactor(Register): Add form action

--- register.php
+++ register.php
@@ -8,7 +8,8 @@
   <body>
     <div>
       <img src="media/ICICT.jpg" alt="LOGO" class="logo">
-    </div>  
+    </div>
+    <!-- Nav -->
     <div class="nav_container">
       <ul class="navbar">
         <li><a href="home.html">Home</a></li>
@@ -18,8 +19,9 @@
         <li><a href="">Contact Us</a></li>
       </ul>
     </div>
-    </br> 
-    <form class="form" method="post">
+    </br>
+    <!-- Signup form -->
+    <form class="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
       <div class="content">
         <div class="top_login">
           <h1>Register Here</h1>
@@ -27,9 +29,7 @@
         <input type="text" name="text" placeholder="username" class="input">
         <input type="email" name="email" placeholder="Email" class="input">
         <input type="password" name="password" placeholder="Password" class="input">
-        <button class="button">Submit</button>
-        <div class="link">
-        </div>
+        <button class="button" type="submit">Submit</button>
       </div>
     </form>
   </div>

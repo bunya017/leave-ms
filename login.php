f4277d13e6250debaf939d53c18fd48c79600965
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-06 13:57:55 +0100

    chore(Login): Rename employee_login.html to login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <html>
   <head>
-    <title>Recovery Password</title>
+    <title>Login</title>
     <link rel="stylesheet" href="static/css/form_log.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>

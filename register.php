37b925fff09ddc1f793b68edc06a7a6631379d6e
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-06 13:41:47 +0100

    refactor(Register): Add signup logic

--- register.php
+++ register.php
@@ -6,6 +6,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
   </head>
   <body>
+    <?php
+      require("config.php");
+      if (isset($_POST["submit"])) {
+        $username = stripcslashes($_POST["username"]);
+        $email = stripcslashes($_POST["email"]);
+        $password = password_hash(
+          stripcslashes($_POST["password"]), PASSWORD_DEFAULT
+        );
+        $query = "INSERT into `users` (username, email, password) VALUES ('$username', '$email', '$password')";
+        if ($conn->query($query) === TRUE) {
+          echo "<br>Created user<br>";
+        } else {
+          echo "<br>" . $conn->error;
+        }
+      }
+    ?>
+
     <div>
       <img src="media/ICICT.jpg" alt="LOGO" class="logo">
     </div>
@@ -21,7 +38,7 @@
     </div>
     </br>
     <!-- Signup form -->
-    <form class="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <form class="form" method="post">
       <div class="content">
         <div class="top_login">
           <h1>Register Here</h1>
@@ -29,7 +46,7 @@
         <input type="text" required="" name="username" placeholder="Username" class="input">
         <input type="email" required="" name="email" placeholder="Email" class="input">
         <input type="password" required="" name="password" placeholder="Password" class="input">
-        <button class="button" type="submit">Submit</button>
+        <button class="button" type="submit" name="submit">Submit</button>
       </div>
     </form>
   </div>

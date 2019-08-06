6fb94608bf5b47ef22f9f28fc57e5a0c4f8c0f5b
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-06 11:25:55 +0100

    refactor(Register): Move code from register.html to register.php

---
+++ register.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+<title>Register</title>
+<link rel="stylesheet" href="static/css/form_log.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+</head>
+<body>
+<div>
+    <img src="media/ICICT.jpg" alt="LOGO" class="logo">
+  </div>  
+  <div class="nav_container">
+    <ul class="navbar">
+      <li><a href="home.html">Home</a></li>
+      
+      <li><a href="register.html" class="regester">Register</a></li>
+      <li><a href="About_Us.html">About Us</a></li>
+      <li><a href="">Contact Us</a></li>
+    </ul>
+  </div>
+ </br> 
+      
+
+
+<form class="form">
+  <div class="content">
+    <div class="top_login"><h1>Register Here</h1></div>
+    <input type="text" placeholder="username" class="input">
+    <input type="email" placeholder="Email" class="input">
+    <input type="password" placeholder="Password" class="input">
+    <input type="password" placeholder="Retype Password" class="input">
+    <button class="button">Submit</button>
+    <div class="link">
+    </div>
+  </div>
+
+  </form>
+</div>
+
+
+
+
+</body>
+</html>

d7201d1c44d538323d3a5334043c6e2938493de0
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-10 11:24:27 +0100

    refactor(Dept): Add alert for successful department creation

--- admin/departments.php
+++ admin/departments.php
@@ -17,6 +17,28 @@
         $query = "SELECT * FROM `departments`";
         $result = $conn->query($query);
       }
+      if (isset($_POST["addDepartment"])) {
+        if ((empty($_POST["name"]) === TRUE) and (empty($_POST["shortCode"]) === TRUE)) {
+          $_SESSION["deptNameError"] = true;
+          $_SESSION["shortCodeError"] = true;
+        } elseif (empty($_POST["shortCode"]) === TRUE) {
+          $_SESSION["shortCodeError"] = true;
+        } elseif (empty($_POST["name"]) === TRUE) {
+          $_SESSION["deptNameError"] = true;
+        } elseif (isset($_POST["name"]) === TRUE and isset($_POST["shortCode"]) === TRUE) {
+          $name = stripcslashes($_POST["name"]);
+          $shortCode = stripcslashes($_POST["shortCode"]);
+          $query = "INSERT into `departments` (name, short_code) VALUES ('$name', '$shortCode')";
+          if ($conn->query($query) === TRUE) {
+            $_SESSION["deptCreated"] = true;
+            $_POST = NULL;
+          } elseif (strpos($conn->error, "'name'") > 0) {
+            $_SESSION["deptNameDupError"] = true;
+          } elseif (strpos($conn->error, "'short_code'") > 0) {
+            $_SESSION["shortCodeDupError"] = true;
+          }
+        }
+      }
     ?>
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
       <a class="navbar-brand ml-auto" href="dashboard.php">
@@ -38,6 +60,14 @@
       <!-- Employee section -->
       <div class="row py-4">
         <div class="col-10 mx-auto">
+          <?php
+            if (isset($_SESSION["deptCreated"])) {
+              if ($_SESSION["deptCreated"] === true) {
+                echo '<div class="alert alert-success alert-dismissible text-center"><button type="button" class="close" data-dismiss="alert">&times;</button>Department added successfully!</div>';
+                $_SESSION["deptCreated"] = NULL;
+              }
+            }
+          ?>
           <!-- Title -->
           <div class="row align-items-center py-3">
             <div class="col-6">
@@ -76,27 +106,6 @@
           </div>
 
           <!-- Add Employee Modal -->
-          <?php
-            if (isset($_POST["addDepartment"])) {
-              if ((empty($_POST["name"]) === TRUE) and (empty($_POST["shortCode"]) === TRUE)) {
-                $_SESSION["deptNameError"] = true;
-                $_SESSION["shortCodeError"] = true;
-              } elseif (empty($_POST["shortCode"]) === TRUE) {
-                $_SESSION["shortCodeError"] = true;
-              } elseif (empty($_POST["name"]) === TRUE) {
-                $_SESSION["deptNameError"] = true;
-              } elseif (isset($_POST["name"]) === TRUE and isset($_POST["shortCode"]) === TRUE) {
-                $name = stripcslashes($_POST["name"]);
-                $shortCode = stripcslashes($_POST["shortCode"]);
-                $query = "INSERT into `departments` (name, short_code) VALUES ('$name', '$shortCode')";
-                if ($conn->query($query) === TRUE) {
-                  echo "<br>Created department<br>";
-                } elseif (strpos($conn->error, "'name'") > 0) {
-                  $_SESSION["deptNameDupError"] = true;
-                }
-              }
-            }
-          ?>
           <div class="modal" id="addDepartmentModal" data-backdrop="static" tabindex="-1">
             <div class="modal-dialog">
               <div class="modal-content">
@@ -115,11 +124,13 @@
                           <input type="text" name="name" value="<?php if (isset($_POST['name'])) {echo($_POST['name']);} ?>"class="form-control">
                           <?php
                             if (isset($_SESSION["deptNameError"])) {
+                              // Catch empty field error
                               if ($_SESSION["deptNameError"] === true) {
                                 echo '<small class="text-danger"><strong>This field is required!</strong></small>';
                                 $_SESSION["deptNameError"] = NULL;
                               }
                             } elseif (isset($_SESSION["deptNameDupError"]) and ($_SESSION["deptNameDupError"] === TRUE)) {
+                              // Catch duplicate name error
                               echo '<small class="text-danger"><strong>This name already exists!</strong></small>';
                                 $_SESSION["deptNameDupError"] = NULL;
                             }
@@ -130,10 +141,15 @@
                           <input type="text" name="shortCode" value="<?php if (isset($_POST['shortCode'])) {echo($_POST['shortCode']);} ?>" class="form-control">
                           <?php
                             if (isset($_SESSION["shortCodeError"])) {
+                              // Catch empty field error
                               if ($_SESSION["shortCodeError"] === true) {
                                 echo '<small class="text-danger"><strong>This field is required!</strong></small>';
                                 $_SESSION["shortCodeError"] = NULL;
                               }
+                            } elseif (isset($_SESSION["shortCodeDupError"]) and ($_SESSION["shortCodeDupError"] === TRUE)) {
+                              // Catch duplicate short_code error
+                              echo '<small class="text-danger"><strong>This name short code exists!</strong></small>';
+                                $_SESSION["shortCodeDupError"] = NULL;
                             }
                           ?>
                         </div>

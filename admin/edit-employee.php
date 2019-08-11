7c4017cd0d8bc8125ee9232a9f5ac9f9b64b916e
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-11 23:15:58 +0100

    refactor(EditEmployee): Add navbar

---
+++ admin/edit-employee.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Employee Profile</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <link rel="stylesheet" href="../static/css/bootstrap.min.css">
+    <script src="../static/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="../static/js/popper.min.js"></script>
+    <script src="../static/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <a class="navbar-brand ml-auto" href="dashboard.php">
+        Leave MS <span class="badge badge-info">Admin</span>
+      </a>
+      <ul class="navbar-nav offset-md-6 offset-1 mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="employees.php">Employees</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="dashboard.php">Leave Applications</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="departments.php">Departments</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="container">
+      <div class="row">
+        <div class="col-10 mx-auto">
+        </div>
+      </div>
+    </div>
+  </body>
+</html>

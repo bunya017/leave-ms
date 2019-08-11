99c5489f76b609bdca04ed5984bf372dd209bc07
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-11 09:47:28 +0100

    refactor(NewEmployee): Add navbar

---
+++ admin/add-employee.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>New Employee</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <link rel="stylesheet" href="../static/css/bootstrap.min.css">
+    <script src="../static/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="../static/js/popper.min.js"></script>
+    <script src="../static/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <a class="navbar-brand ml-auto" href="employee-leave.html">
+        Leave MS <span class="badge badge-info">Admin</span>
+      </a>
+      <ul class="navbar-nav offset-md-6 offset-1 mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="">Employees</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="dashboard.php">Leave Applications</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="departments.php">Departments</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="container">
+    </div>
+  </body>
+</html>

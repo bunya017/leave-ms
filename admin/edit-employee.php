d680dc8e1b06084ee1e2a3e5e8e9b91b432bdd88
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-12 12:45:02 +0100

    refactor(EdtEmployee): Add server side validation to catch empty form fields

--- admin/edit-employee.php
+++ admin/edit-employee.php
@@ -9,6 +9,39 @@
     if (($result->num_rows > 0)){
       $staff = $result->fetch_assoc();
     }
+    if (isset($_POST['editEmployee'])) {
+      if (empty($_POST["staff_pin"])) {
+        $_SESSION["staffUpdateError"] = true;
+      }
+      if (empty($_POST["email"])) {
+        $_SESSION["emailUpdateError"] = true;
+      }
+      if (empty($_POST["department"])) {
+        $_SESSION["deptUpdateError"] = true;
+      }
+      if (empty($_POST["first_name"])) {
+        $_SESSION["firstNameUpdateError"] = true;
+      }
+      if (empty($_POST["last_name"])) {
+        $_SESSION["lastNameUpdateError"] = true;
+      }
+    }
+    if (isset($_POST['editEmployee'], $_POST["staff_pin"], $_POST["department"],
+      $_POST["email"], $_POST["first_name"], $_POST["last_name"])
+    ) {
+      $staff_pin = stripcslashes($_POST['staff_pin']);
+      $email = stripcslashes($_POST['email']);
+      $first_name = stripcslashes($_POST['first_name']);
+      $last_name = stripcslashes($_POST['last_name']);
+      $department = stripcslashes($_POST['department']);
+      $updateQuery = "UPDATE `users` SET staff_pin='$staff_pin', email='$email',
+        first_name='$first_name', last_name='$last_name', department_id='$department' WHERE staff_pin='$staffPin'";
+      if ($conn->query($updateQuery) === TRUE) {
+        echo "Updated!";
+      } else {
+        echo "<br>" . $conn->error . "<br>";
+      }
+    }
   }
 ?>
 <!DOCTYPE html>
@@ -55,18 +88,46 @@
                     <div class="form-group">
                       <label>Staff Pin:</label>
                       <input type="text" name="staff_pin" value="<?php if(isset($_POST['staff_pin'])){echo($_POST['staff_pin']);}else{echo($staff['staff_pin']);} ?>" required="" class="form-control">
+                      <?php
+                        // Catch empty field error
+                        if (isset($_SESSION["staffUpdateError"]) && ($_SESSION["staffUpdateError"] === true)) {
+                          echo '<small class="text-danger"><strong>This field is required!</strong></small>';
+                          $_SESSION["staffUpdateError"] = NULL;
+                        }
+                      ?>
                     </div>
                     <div class="form-group">
                       <label>Email:</label>
                       <input type="email" name="email" value="<?php if(isset($_POST['email'])){echo($_POST['email']);}else{echo($staff['email']);} ?>" required="" class="form-control">
+                      <?php
+                        // Catch empty field error
+                        if (isset($_SESSION["emailUpdateError"]) && ($_SESSION["emailUpdateError"] === true)) {
+                          echo '<small class="text-danger"><strong>This field is required!</strong></small>';
+                          $_SESSION["emailUpdateError"] = NULL;
+                        }
+                      ?>
                     </div>
                     <div class="form-group">
                       <label>First Name:</label>
                       <input type="text" name="first_name" value="<?php if(isset($_POST['first_name'])){echo($_POST['first_name']);}else{echo($staff['first_name']);} ?>" required="" class="form-control">
+                      <?php
+                        // Catch empty field error
+                        if (isset($_SESSION["firstNameUpdateError"]) && ($_SESSION["firstNameUpdateError"] === true)) {
+                          echo '<small class="text-danger"><strong>This field is required!</strong></small>';
+                          $_SESSION["firstNameUpdateError"] = NULL;
+                        }
+                      ?>
                     </div>
                     <div class="form-group">
                       <label>Last Name:</label>
                       <input type="text" name="last_name" value="<?php if(isset($_POST['last_name'])){echo($_POST['last_name']);}else{echo($staff['last_name']);} ?>" required="" class="form-control">
+                      <?php
+                        // Catch empty field error
+                        if (isset($_SESSION["lastNameUpdateError"]) && ($_SESSION["lastNameUpdateError"] === true)) {
+                          echo '<small class="text-danger"><strong>This field is required!</strong></small>';
+                          $_SESSION["lastNameUpdateError"] = NULL;
+                        }
+                      ?>
                     </div>
                     <div class="form-group">
                       <label>Department:</label>
@@ -86,6 +147,13 @@
                           }
                         ?>
                       </select>
+                      <?php
+                        // Catch empty field error
+                        if (isset($_SESSION["deptUpdateError"]) && ($_SESSION["deptUpdateError"] === true)) {
+                          echo '<small class="text-danger"><strong>This field is required!</strong></small>';
+                          $_SESSION["deptUpdateError"] = NULL;
+                        }
+                      ?>
                     </div>
                     <div class="modal-footer border-0">
                       <button type="submit" name="editEmployee" class="btn btn-dark">

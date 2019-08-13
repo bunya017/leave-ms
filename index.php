a7453ce467e3f7a7d70962333280a015c436d164
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-13 22:52:57 +0100

    refactor(Login): Add user_id to session on login

--- index.php
+++ index.php
@@ -31,6 +31,7 @@
               $_SESSION["first_name"] = $row["first_name"];
               $_SESSION["last_name"] = $row["last_name"];
               $_SESSION["staff_pin"] = $row["staff_pin"];
+              $_SESSION["user_id"] = $row["id"];
               $_SESSION["isLoggedIn"] = true;
               $role_id = $row["role_id"];
               $role_sql = "SELECT * FROM `roles` WHERE id='$role_id'";

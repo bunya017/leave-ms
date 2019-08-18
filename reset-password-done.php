da895046797c22c16b3c06440905de7d78ce64b9
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-18 19:56:26 +0100

    chore: Add reset-password-done.php

---
+++ reset-password-done.php
@@ -0,0 +1,44 @@
+<html>
+  <head>
+    <title>Password Reset Successfully!</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <link rel="stylesheet" href="static/css/bootstrap.min.css">
+    <script src="static/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="static/js/popper.min.js"></script>
+    <script src="static/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <a class="navbar-brand ml-5" href="index.php">
+        Leave MS
+      </a>
+    </nav>
+    <div class="container">
+      <div class="row py-5">
+        <div class="col-6 mx-auto">
+          <div class="card border-0 shadow-lg">
+            <div class="card-body">
+              <div class="row pb-5 pt-4">
+                <div class="col-11 mx-auto">
+                  <div class="py-3">
+                    <h2 class="text-center">Password Reset Successful!</h2>
+                  </div>
+                  <div class="pt-2">
+                    <p class="pb-3">Your password has been changed successfully! Please login to contnue.</p>
+                    <a class="btn btn-dark btn-sm" href="index.php">LOGIN</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+  <style>
+    body {
+      background-color: #f4f3f4;
+    }
+  </style>
+</htm

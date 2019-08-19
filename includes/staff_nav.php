36748393029bc5ac1e3bc29e1154cc9dbe11ed5d
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-19 20:05:34 +0100

    refactor(StaffNav): Add account dropdown

--- includes/staff_nav.php
+++ includes/staff_nav.php
@@ -3,11 +3,16 @@
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
       <a class="navbar-brand ml-auto" href="dashboard.php">Leave MS</a>
       <ul class="navbar-nav offset-md-8 offset-1 mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="profile.php">My profile</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../logout.php">Logout</a>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Account
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="profile.php">My Profile</a>
+            <a class="dropdown-item" href="change-password.php">Change password</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="../logout.php">Logout</a>
+          </div>
         </li>
       </ul>
     </nav>

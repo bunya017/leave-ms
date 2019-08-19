05c9fb85f4ea144c5ccc4575f3fdccc324a66591
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-19 22:51:29 +0100

    refactor: Add includes/home_nav.php

---
+++ includes/home_nav.php
@@ -0,0 +1,9 @@
+<?php
+  echo '
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <a class="navbar-brand ml-5" href="index.php">
+        Leave MS
+      </a>
+    </nav>
+  ';
+?>
